670df7a264851d8243a59ae1a458f09d48849770
bewokz <kdhp38@gmail.com>
2022-10-12 12:35:43 +0700

    aaa

--- resources/views/controls/admins/add-properties.blade.php
+++ resources/views/controls/admins/add-properties.blade.php
@@ -3,49 +3,48 @@
 <div class="container mt-5">
     <h1>Tambah Produk</h1>
     <form action="/add-properties" method="POST" enctype="multipart/form-data" class="form">
-        @csrf
+      @csrf
         <div class="form-row">
-            <div class="form-group col-6">
-                <label for="properties_name" class="form-label">Properties Name</label>
-                <input type="text" class="form-control" id="properties_name" name="properties_name" placeholder="Tuliskan nama Produk" required>
-            </div>
-
-            <div class="form-group col-6">
-                <label for="type" class="form-label">Type Home</label>
-                <input type="text" class="form-control" id="type" name="type" placeholder="Tuliskan nama Produk" required>
-            </div>
+          <div class="form-group col-6">
+            <label for="inputProperties">Properties Name</label>
+            <input type="properties" class="form-control" id="properties_name" name="properties_name" placeholder="Tuliskan Nama Produk">
+          </div>
+          <div class="form-group col-6">
+            <label for="inputTypeHome">Type Home</label>
+            <input type="Home" class="form-control" id="type" name="type" placeholder="Tuliskan Tipe Rumah">
+          </div>
         </div>
-
-        <div class="form-group col-12">
-            <label for="location" class="form-label">Location</label>
-            <input type="text" class="form-control" id="location" name="location" placeholder="Tuliskan nama Produk">
+        <div class="form-group">
+          <label for="inputAddress">Address</label>
+          <input type="text" class="form-control" id="location" name="location" placeholder="Tuliskan Alamat Rumah">
         </div>
-
         <div class="form-row">
             <div class="form-group col-12">
-                <label for="image">Image</label>
+                <label for="exampleFormControlFile1">Home Image</label>
                 <input type="file" class="form-control" accept="image/*" id="image" name="image[]" multiple>
-            </div>
-
+              </div>
             <div class="form-group col-12">
-                <label for="properties_description" class="form-label">Properties Description</label>
-                <textarea class="form-control" rows="4" id="properties_description" name="properties_description" placeholder="Tuliskan nama Produk" required></textarea>
+                <label for="exampleFormControlTextarea1">Properties Description</label>
+                <textarea class="form-control" id="properties_description" name="properties_description" rows="4"></textarea>
             </div>
         </div>
-
         <div class="form-row">
             <div class="form-group col-6">
-                <label for="price" class="form-label">Price</label>
-                <input type="number" class="form-control" id="price" name="price" placeholder="Tuliskan nama Produk" required>
+              <label for="inputPrice">Price</label>
+              <input type="price" class="form-control" id="price" name="price" placeholder="Tuliskan Harga Produk">
             </div>
-
             <div class="form-group col-6">
-                <label for="notelp" class="form-label">No. Telp</label>
-                <input type="number" class="form-control" id="notelp" name="notelp" placeholder="No. Telp yang dapat di hubungi">
+              <label for="inputPhoneNumber">Phone Number</label>
+              <input type="Phone Number" class="form-control" id="notelp" name="notelp" placeholder="Tuliskan Nomor Telepon">
             </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Simpan</button>
-    </form>
-</div>
+          </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        
 
 @include('controls/admins.outline.footer')
--- resources/views/controls/admins/outline/header.blade.php
+++ resources/views/controls/admins/outline/header.blade.php
@@ -4,7 +4,7 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -14,7 +14,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="css/control/control.css" rel="stylesheet">
 </head>
 
